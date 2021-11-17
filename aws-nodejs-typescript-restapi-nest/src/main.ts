2ac3a30e9b34d8f8be17484172194d5122d146a4
Haiko <hvdschaaf@gmail.com>
2021-11-17 15:27:42 +0100

    upgrade nestjs

--- aws-nodejs-typescript-restapi-nest/src/main.ts
+++ aws-nodejs-typescript-restapi-nest/src/main.ts
@@ -3,8 +3,11 @@ import { Server } from 'http';
 import { createServer, proxy } from 'aws-serverless-express';
 import { eventContext } from 'aws-serverless-express/middleware';
 
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+import * as express from 'express';
 
 // NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
 // due to a compressed response (e.g. gzip) which has not been handled correctly
@@ -15,18 +18,20 @@ const binaryMimeTypes: string[] = [];
 let cachedServer: Server;
 
 process.on('unhandledRejection', (reason) => {
+  // tslint:disable-next-line:no-console
   console.error(reason);
 });
 
 process.on('uncaughtException', (reason) => {
+  // tslint:disable-next-line:no-console
   console.error(reason);
 });
 
 async function bootstrapServer(): Promise<Server> {
   if (!cachedServer) {
     try {
-      const expressApp = require('express')();
-      const nestApp = await NestFactory.create(AppModule, expressApp);
+      const expressApp = express();
+      const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
       nestApp.enableCors();
       nestApp.use(eventContext());
       await nestApp.init();
