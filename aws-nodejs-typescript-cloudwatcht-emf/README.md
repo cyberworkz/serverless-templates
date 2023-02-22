68f4b7b0968ddf8d71efd9edba3f3f32737f1c04
Haiko van der Schaaf <hvdschaaf@gmail.com>
2023-02-22 16:07:22 +0100

    add cloudwatch-emf template

---
+++ aws-nodejs-typescript-cloudwatcht-emf/README.md
@@ -0,0 +1,25 @@
+<!--
+title: 'AWS REST API Template (NodeJS & Typescript)'
+description: 'This template sets up an AWS Lambda with Typescript for a REST API using NestJS and express'
+platform: AWS
+language: TypeScript
+authorLink: 'https://github.com/cyberworkz/serverless-templates'
+authorName: 'Haiko van der Schaaf'
+-->
+# Serverless NestJS REST API template
+
+This serverless template enables you to run a NestJS REST API with AWS Lambda using Serverless and TypeScript.
+
+## Use Cases
+- REST API.
+
+## Setup
+1. Install serverless - ```npm install -g serverless```
+2. Use serverless to create a project with template-url. For example
+   ```serverless create --template-url https://github.com/cyberworkz/serverless-templates/tree/main/aws-nodejs-typescript-restapi-nest --path myService```
+   
+
+## Examples
+See the following article for a step-to-step guide how to use this template https://medium.com/dev-genius/how-to-build-a-serverless-rest-api-with-nestjs-and-dynamodb-7b58b5b59bf6
+
+
---
+++ aws-nodejs-typescript-cloudwatcht-emf/src/main.ts
@@ -0,0 +1,50 @@
+import { Handler, Context } from 'aws-lambda';
+import { Server } from 'http';
+import { createServer, proxy } from 'aws-serverless-express';
+import { eventContext } from 'aws-serverless-express/middleware';
+
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import * as express from 'express';
+
+// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
+// due to a compressed response (e.g. gzip) which has not been handled correctly
+// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
+// binaryMimeTypes below
+const binaryMimeTypes: string[] = [];
+
+let cachedServer: Server;
+
+process.on('unhandledRejection', (reason) => {
+  // tslint:disable-next-line:no-console
+  console.error(reason);
+});
+
+process.on('uncaughtException', (reason) => {
+  // tslint:disable-next-line:no-console
+  console.error(reason);
+});
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    try {
+      const expressApp = express();
+      const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+      nestApp.enableCors();
+      nestApp.use(eventContext());
+      await nestApp.init();
+      cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+    }
+    catch (error) {
+      return Promise.reject(error);
+    }
+  }
+  return Promise.resolve(cachedServer);
+}
+
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+}
